8e8dcf4e5d03f8b220b1a2389f64eaa1dd9ba0e8
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-06-10 11:53:04 +0200

    10.06.23

---
+++ Projekt/main.cpp
@@ -0,0 +1,20 @@
+//
+//
+//
+//
+//  @ Project : Inżynieria Oprogramowania - Projekt
+//  @ File Name : main.cpp
+//  @ Date : 10.06.2023
+//  @ Author : Tomasz Wnuk, Bartosz Szynkaruk, Mikołaj Hasiec
+//
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
